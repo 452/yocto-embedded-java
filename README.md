830270b67a9ba2d01972eeeb7119004b694fc8f2
emetemunoy <sp.titan@gmail.com>
2019-09-13 08:02:32 +0300

    improved example

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# yocto embedded java example
+# Yocto Embedded Java example
 
+## Embedded Java minimal image for raspberry pi 3
+Example how to build minimal size bootable image for raspberry pi 3 with installed openjdk-8 Java programming language, used yocto project.
+git branch warrior
+DISTRO_VERSION 2.7.1
+SSTATE_MIRRORS sstate.yoctoproject.org/2.7.1
 ```sh
-cd docker
-docker build -t embedded-java .
-docker run -i embedded-java
+mkdir downloads builded-images;chmod 777 downloads builded-images
+docker build -f docker/embedded-java-minimal-example/Dockerfile -t embedded-java docker/embedded-java-minimal-example
+docker run --rm -it -v $(pwd)/downloads:/downloads -v $(pwd)/builded-images --name embedded-java embedded-java
 ```
+
+## How to install Docker
+https://docs.docker.com/install
---
+++ docker/embedded-java-minimal-example/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source poky/oe-init-build-env
+bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-java
+bitbake-layers add-layer ../meta-openembedded/meta-python
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+
+sed -i 's/qemux86/raspberrypi3-64/g' conf/local.conf
+
+cat >> conf/local.conf <<'EOL'
+LICENSE_FLAGS_WHITELIST_append="commercial_faad2"
+DISTRO_FEATURES_append=" polkit"
+DL_DIR ?= "/downloads"
+SSTATE_MIRRORS ?= "\
+file://.* http://sstate.yoctoproject.org/dev/PATH;downloadfilename=PATH \n \
+file://.* http://sstate.yoctoproject.org/2.7.1/PATH;downloadfilename=PATH \n \
+file://.* http://sstate.yoctoproject.org/2.8/PATH;downloadfilename=PATH \n \
+"
+PREFERRED_PROVIDER_virtual/java-initial-native = "cacao-initial-native"
+PREFERRED_PROVIDER_virtual/java-native = "cacao-native"
+PREFERRED_PROVIDER_virtual/javac-native = "ecj-bootstrap-native"
+IMAGE_INSTALL_append = " openjdk-8"
+EOL
+
+bitbake -k core-image-minimal
+cp -r tmp/deploy/images/* /builded-images
